53c6817f681cab876a39048bfb149d1c7d90cc54
longtony <qlongbui153@gmail.com>
2023-11-26 23:03:18 +0700

    Update config.yml of circleci

--- bash_script/create_cluster.sh
+++ bash_script/create_cluster.sh
@@ -1,6 +1,6 @@
 #!/user/bin/env/bash
 
-cluster_name=long-capstone-project
+cluster_name=capstone-project-cluster
 aws_region=us-east-1
 
 if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_name} | grep -q ${CLUSTER_NAME}; then
