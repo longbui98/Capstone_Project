e00dd1fe958177b6d31c993d4a906d8122ef7d33
longtony <qlongbui153@gmail.com>
2023-11-26 19:12:07 +0700

    Correct path to create ECR  in circleci

--- bash_script/create_ecr.sh
+++ bash_script/create_ecr.sh
@@ -1,10 +1,7 @@
 #!/user/bin/env/bash
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-
-echo $AWS_DEFAULT_REGION
+export AWS_DEFAULT_REGION=us-east-1
+export PROJECT_NAME=capstone-project
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin 717010040964.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
