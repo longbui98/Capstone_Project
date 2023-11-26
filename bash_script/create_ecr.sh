06c62fb4bb7d4aa7b839c75ee8229f7e46cdda69
longtony <qlongbui153@gmail.com>
2023-11-26 18:17:17 +0700

    Update workflows in circleci

--- bash_script/create_ecr.sh
+++ bash_script/create_ecr.sh
@@ -4,7 +4,7 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin 717010040964.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin 717010040964.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 aws ecr create-repository \
     --repository-name $PROJECT_NAME \
