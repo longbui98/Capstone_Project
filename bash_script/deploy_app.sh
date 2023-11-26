629175c96ec9b5a257ca07536ee065cbf2b1abc6
longtony <qlongbui153@gmail.com>
2023-11-26 22:29:21 +0700

    Add job deploy app after creat cluster

---
+++ bash_script/deploy_app.sh
@@ -0,0 +1,17 @@
+#!/user/bin/env/bash
+
+# Get Nodes details
+echo "############### Node details ###############"
+/.kubectl get nodes
+
+# Create the deployment and loadbalancer
+echo "############### Deploy app ###############"
+/.kubectl apply -f manifests/deploy.yml
+
+# # Get details for loadbalancer
+echo "############### Loadbalancer details ###############"
+/.kubectl get service/loadbalancer-service
+
+# # Get details for deployment
+echo "############### Deploy, RS, SVC, Pod details ###############"
+/.kubectl get deploy,rs,svc,pods
