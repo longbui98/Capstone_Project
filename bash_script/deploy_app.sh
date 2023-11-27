ba0417d1ce92c79e14c907dec890d777784b5dda
longtony <qlongbui153@gmail.com>
2023-11-27 22:48:59 +0700

    Update script to deploy_app in config.yml

--- bash_script/deploy_app.sh
+++ bash_script/deploy_app.sh
@@ -2,16 +2,16 @@
 
 # Get Nodes details
 echo "############### Node details ###############"
-/.kubectl get nodes
+kubectl get nodes
 
 # Create the deployment and loadbalancer
 echo "############### Deploy app ###############"
-/.kubectl apply -f deploy.yml
+kubectl apply -f deploy.yml
 
 # # Get details for loadbalancer
 echo "############### Loadbalancer details ###############"
-/.kubectl get service/loadbalancer-service
+kubectl get service/loadbalancer-service
 
 # # Get details for deployment
 echo "############### Deploy, RS, SVC, Pod details ###############"
-/.kubectl get deploy,rs,svc,pods
+kubectl get deploy,rs,svc,pods
