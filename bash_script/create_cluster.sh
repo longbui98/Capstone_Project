599a9a8cbab6ffb14c6f1a7546b93724028f79b5
longtony <qlongbui153@gmail.com>
2023-11-29 00:29:15 +0700

    Update script create_cluster.sh

--- bash_script/create_cluster.sh
+++ bash_script/create_cluster.sh
@@ -8,7 +8,6 @@ if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_nam
 else
     echo "############### Creating Cluster ###############"
     /.eksctl create cluster --name ${cluster_name} --region=${aws_region}
-    echo "Created Cluster Successfully!"
     echo "############### Node Details ###############"
     /.kubectl get nodes
 fi
