10f7129e63a0ff51cc139734f830cfc0aa5ecc4e
longtony <qlongbui153@gmail.com>
2023-12-01 12:32:23 +0700

    Check hadolint in case app.py has error

--- app.py
+++ app.py
@@ -19,11 +19,6 @@ def scale(payload):
     return scaled_adhoc_predict
 
 @app.route("/")
-def home2():
-    html = ""
-    return html.format(format)
-    
-@app.route("/")
 def home():
     html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
