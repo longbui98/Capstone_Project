d696f160854a635e63716f0e9ac02a2d370c0d50
longtony <qlongbui153@gmail.com>
2023-12-02 22:59:08 +0700

    Update region when push image to docker hub

--- bash_script/deploy_app.sh
+++ bash_script/deploy_app.sh
@@ -6,7 +6,11 @@ kubectl get nodes
 
 # Create the deployment and loadbalancer
 echo "############### Deploy app ###############"
-kubectl create deployment longbui-api --image=longtony/longbui-api:v1.0.0 && kubectl expose deployment/longbui-api:v1.0.0 --type="LoadBalancer" --port 80
+kubectl create deployment longbui-api --image=longtony/longbui-api:v1.0.0
+
+# Forward the container port to a host
+echo "############### Forward port to a host ###############"
+kubectl port-forward longtony/longbui-api:v1.0.0 8000:80
 
 # Get details for deployment
 echo "############### Deploy, RS, SVC, Pod details ###############"
