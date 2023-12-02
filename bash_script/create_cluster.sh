7ab2a38ebf98d75f420ff3453644e6cc6a596e8c
longtony <qlongbui153@gmail.com>
2023-12-02 22:19:20 +0700

    Update region when creating cluster

--- bash_script/create_cluster.sh
+++ bash_script/create_cluster.sh
@@ -1,13 +1,13 @@
 #!/user/bin/env/bash
 
 cluster_name=capstone-project-cluster
-aws_region=us-east-1
+aws_region=us-east-2
 
 if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_name} | grep -q ${CLUSTER_NAME}; then
     echo "Cluster already exists!"
 else
     echo "############### Creating Cluster ###############"
-    /.eksctl create cluster --name long-capstone-project --region us-east-1 --fargate
+    /.eksctl create cluster --name long-capstone-project --region us-east-2 --fargate
     echo "############### Node Details ###############"
     /.kubectl get nodes
 fi
