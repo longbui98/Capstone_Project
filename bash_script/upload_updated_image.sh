b1a621b28acc5080bdf510b1a3b9d06016b861d8
longtony <qlongbui153@gmail.com>
2023-12-02 21:42:28 +0700

    Update region when creating cluster

--- bash_script/upload_updated_image.sh
+++ bash_script/upload_updated_image.sh
@@ -1,6 +1,6 @@
 #!/user/bin/env/bash
 
-export AWS_DEFAULT_REGION=us-east-2
+export AWS_DEFAULT_REGION=us-east-1
 export PROJECT_NAME=capstone-project
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin 717010040964.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com	
