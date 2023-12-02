006b42f7e1c341ea0fac51fe1b8a067fc1fb98b6
longtony <qlongbui153@gmail.com>
2023-12-02 20:12:21 +0700

    Update command to push dockerhub and deploy app

--- bash_script/deploy_app.sh
+++ bash_script/deploy_app.sh
@@ -6,7 +6,7 @@ kubectl get nodes
 
 # Create the deployment and loadbalancer
 echo "############### Deploy app ###############"
-kubectl create deployment longbui-api --image=longtony/longbui-api:v1.0.0 && kubectl expose deployment/longbui-api --type="LoadBalancer" --port 80
+kubectl create deployment longbui-api --image=longtony/longbui-api:v1.0.0 && kubectl expose deployment/longbui-api:v1.0.0 --type="LoadBalancer" --port 80
 
 # Get details for deployment
 echo "############### Deploy, RS, SVC, Pod details ###############"
