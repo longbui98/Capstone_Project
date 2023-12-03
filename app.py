32bb79ec3c9303b1a3806702102ca2e7fe71e036
longtony <qlongbui153@gmail.com>
2023-12-03 19:34:40 +0700

    Update command to build image from Dockerfile

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
 
-import pandas as pd
-import joblib
-from sklearn.preprocessing import StandardScaler
-
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
