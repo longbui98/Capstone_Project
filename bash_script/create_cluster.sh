b10d17721a3fff559f29160cfd79d3d4b8a89a13
longtony <qlongbui153@gmail.com>
2023-12-03 18:00:49 +0700

    Update job to create eks cluster

--- bash_script/create_cluster.sh
+++ bash_script/create_cluster.sh
@@ -1,13 +1,13 @@
 #!/user/bin/env/bash
 
-cluster_name=capstone-project-cluster
+cluster_name=hello-cluster
 aws_region=us-east-2
 
 if /.eksctl utils describe-stacks --region=${aws_region} --cluster=${cluster_name} | grep -q ${CLUSTER_NAME}; then
     echo "Cluster already exists!"
 else
     echo "############### Creating Cluster ###############"
-    /.eksctl create cluster --name long-capstone-project --region us-east-2 --fargate
+    /.eksctl create cluster -f cluster_deploy/eksctl-cluster.yaml
     echo "############### Node Details ###############"
     /.kubectl get nodes
 fi
