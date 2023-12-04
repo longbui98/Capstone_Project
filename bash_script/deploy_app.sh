4ccb5a978ddda8dc4972bd923d03ee97ee13d00d
longtony <qlongbui153@gmail.com>
2023-12-04 16:18:52 +0700

    Update script to deploy app to eks

--- bash_script/deploy_app.sh
+++ bash_script/deploy_app.sh
@@ -10,11 +10,7 @@ kubectl get nodes
 
 # Create the deployment and loadbalancer
 echo "############### Deploy app ###############"
-kubectl create deployment longbui-api --image=longtony/longbui-api:v1.0.0
-
-# Forward the container port to a host
-echo "############### Forward port to a host ###############"
-kubectl expose deployment/longbui-api --type="LoadBalancer" --port=8080 --target-port=8080
+kubectl apply -f cluster_deploy/deployment.yml
 
 # Get details for deployment
 echo "############### Deploy, RS, SVC, Pod details ###############"
